61c2937b6976846588067650a811134ea8f641dc
ramsteven <ramsteven@gmail.com>
2020-12-08 19:05:39 +0000

    adding new formats3

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-digit *= -1
+digit = number % 10
+if number < 0:
+    digit = abs(number) % 10
 print("Last digit of", end=" ")
 if digit > 5:
     print("{:d} is {:d} and is greater than 5".format(number, digit))

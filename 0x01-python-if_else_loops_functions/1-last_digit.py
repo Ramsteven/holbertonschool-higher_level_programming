30fda895f5f645f29f25350f6a604de8dceb8de0
ramsteven <ramsteven@gmail.com>
2020-12-08 18:43:25 +0000

    changing thing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 neoNumber = abs(number) % 10
-print("Last digit of {:d} is {:d}".format(number, neoNumber), end=" ")
+print("Last digit of {:d} is {:d}".format(number, neoNumber), end="")
 if neoNumber > 5:
-    print("and is greater than 5")
+    print(" and is greater than 5")
 elif neoNumber == 0:
-    print("and is 0")
+    print(" and is 0")
 else:
-    print("and is less than 6 and not 0")
+    print(" and is less than 6 and not 0")

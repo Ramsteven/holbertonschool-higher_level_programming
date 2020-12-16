d4037d85a6709c8bbc4d01208f3cadd4628b315d
ramsteven <ramsteven@gmail.com>
2020-12-16 18:46:33 +0000

    adding task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in matrix:
+        space = ""
+        for number in x:
+            print("{:s}{:d}".format(space, number), end="")
+        print()

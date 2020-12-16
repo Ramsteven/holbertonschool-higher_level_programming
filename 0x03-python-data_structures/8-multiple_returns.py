407173f7a330a29a0bbb61a11aa616268c6d1198
ramsteven <ramsteven@gmail.com>
2020-12-16 20:58:03 +0000

    adding files 1

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return None
+        return 0, None
     else:
         tuple_new = (len(sentence), sentence[0])
         return tuple_new

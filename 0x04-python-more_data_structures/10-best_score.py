4ddfae9622675c027291fa92dd6eed075db01935
ramsteven <ramsteven@gmail.com>
2020-12-17 19:41:48 +0000

    adding task 01

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or a_dictionary == {}:
         return None
     score = 0
     key_list = list(a_dictionary.keys())

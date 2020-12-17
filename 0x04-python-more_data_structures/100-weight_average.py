455a529df93cf548c8901b2f0284ff34700752f4
ramsteven <ramsteven@gmail.com>
2020-12-17 22:00:10 +0000

    adding task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    suma = 0
+    secon_sum = 0
+    for i in my_list:
+        mul = 1
+        n = 0
+        for y in i:
+            mul *= y
+            n += 1
+            if n == 2:
+                secon_sum += y
+        suma += mul
+
+    return suma/secon_sum

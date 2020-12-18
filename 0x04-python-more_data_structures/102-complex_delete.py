c010987a2be0317ed0ca5edf98a0d0d8def149b9
ramsteven <ramsteven@gmail.com>
2020-12-18 01:48:46 +0000

    adding task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,7 +4,6 @@ def complex_delete(dic, value):
     keys = list(dic.keys())
     for i in values:
         if i == value:
-            position = values.index(value)
-            key = keys[position]
-            dic.pop(key, 0)
+            position = values.index(i)
+            dic.pop(keys[position], 0)
     return dic

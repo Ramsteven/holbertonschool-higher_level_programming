b373be03c33d505818aeb817ed25a0ac85f52493
ramsteven <ramsteven@gmail.com>
2021-01-04 14:50:04 +0000

    addig task 5

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    for n in range(0, x):
+        try:
+                print(my_list[n], end="")
+        except:
+            break
+    print()
+    return n

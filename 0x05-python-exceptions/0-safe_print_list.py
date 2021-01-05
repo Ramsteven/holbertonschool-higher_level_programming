36777dfab04394127ee109bb9e68a497531c5127
ramsteven <ramsteven@gmail.com>
2021-01-05 02:29:44 +0000

    adding counter

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     for n in range(0, x):
         try:
                 print(my_list[n], end="")
+                i += 1
         except:
             break
     print()
-    return n
+    return i

b90cfce5a11a2a66489e5fc26c43a113ca2397ea
ramsteven <ramsteven@gmail.com>
2021-01-05 23:55:40 +0000

    adding task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,23 +3,21 @@
 
 
 class Square:
+
     '''init square and raising errors'''
     def __init__(self, size=0, position=(0, 0)):
-        if type(size) is not int or \
-           type(position[0]) is not int or \
-           type(position[1]) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0 or position[0] < 0 or position[1] < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
+
     '''get value area'''
     def area(self):
         return self.__size**2
+
     '''setter size '''
     @property
     def size(self):
         return self.__size
+
     '''set size'''
     @size.setter
     def size(self, value):
@@ -29,6 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     ''' print ouput of the square '''
     def my_print(self):
         if self.size == 0:
@@ -36,16 +35,18 @@ class Square:
         else:
             for x in range(self.__position[1]):
                 print()
-            for y in range(0, self.__size):
-                for x in range(0, self.__position[0]):
+            for y in range(self.__size):
+                for x in range(self.__position[0]):
                     print(" ", end="")
-                for x in range(0, self.__size):
+                for x in range(self.__size):
                     print("#", end="")
                 print()
+
     '''position getter'''
     @property
     def position(self):
         return self.__position
+
     ''' position setter'''
     @position.setter
     def position(self, value):

3a8aca127d9980b12676ff19fbe22fbc2bbed5a4
ramsteven <ramsteven@gmail.com>
2021-01-07 15:17:19 +0000

    adding task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,8 +5,12 @@
 class Square:
     '''init square and raising errors'''
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+        self.position = position
 
     '''get value area'''
     def area(self):
@@ -48,9 +52,9 @@ class Square:
     ''' position setter'''
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or type(value[1]) is not int or \
-           type(value[0]) is not int or value[0] < 0 or value[1] < 0 or \
-           len(value) != 2:
+        if len(value) != 2 or type(value) is not tuple or \
+           type(value[1]) is not int or \
+           type(value[0]) is not int or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value

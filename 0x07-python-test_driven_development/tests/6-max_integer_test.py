16df819393a5617488b85801a39a427f785e6621
ramsteven <ramsteven@gmail.com>
2021-01-09 19:01:29 +0000

    addig function tester

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,4 +1,4 @@
-#!usr/bin/phyton3
+#!/usr/bin/python3
 """
     Unittest for max_integer([..])
 """
@@ -14,19 +14,18 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_integer(self):
-        self.assertEqual(max_integer([2, 4, 5, 6, 7]), 7)
+        "Test numbers cases"
+        self.assertEqual(max_integer([7, 4, 5, 6, 2]), 7)
         self.assertEqual(max_integer([2, 4, 200, 6, 7]), 200)
-        self.assertEqual(max_integer([2, 4, 5, 100, 7]), 100)
-        self.assertEqual(max_integer([2, 4, 5, 6, 323]), 323)
+        self.assertEqual(max_integer([-2, -4, -5, -100, 0]), 0)
+        self.assertEqual(max_integer([2.3, 4.6, 5.3, 6, 32.3]), 32.3)
+        self.assertEqual(max_integer([45]), 45)
 
     def test_void(self):
+        "Test empty"
         self.assertEqual(max_integer([]), None)
 
     def test_raise(self):
-        self.assertRaises(TypeError, max_integer(), None)
-        self.assertRaises(TypeError, max_integer(), 5)
-        self.assertRaises(TypeError, max_integer(), {2, 8, 9})
-
-
-if __name__ == '__main__':
-    unittest.main()
+        "Test error cases"
+        self.assertRaises(TypeError, max_integer, None)
+        self.assertRaises(TypeError, max_integer, {2, 8, 9})

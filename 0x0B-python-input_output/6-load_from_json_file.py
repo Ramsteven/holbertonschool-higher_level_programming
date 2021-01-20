37cef71bfb4e419b3360e8ac2d6ffa5d3e1a252d
ramsteven <ramsteven@gmail.com>
2021-01-20 23:41:55 +0000

    change import below of header

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-""" module load from json"""
 import json
+""" module load from json"""
 
 
 def load_from_json_file(filename):
     """ method load from jsonfile"""
     with open(filename, encoding="utf-8") as file_name:
-        return json.load(file_name)
+        return json.loads(file_name)

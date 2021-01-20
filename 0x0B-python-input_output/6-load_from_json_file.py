a3706ce4c4bb18e12aa5dfb89f4860cf50835625
ramsteven <ramsteven@gmail.com>
2021-01-20 23:44:00 +0000

    adding task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
-import json
 """ module load from json"""
 
 
 def load_from_json_file(filename):
     """ method load from jsonfile"""
+    import json
     with open(filename, encoding="utf-8") as file_name:
-        return json.loads(file_name)
+        return json.load(file_name)

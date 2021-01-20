774d1d1040964dacf0b6a12612e292ff47f2bc9d
ramsteven <ramsteven@gmail.com>
2021-01-20 23:56:40 +0000

    change postion of import

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -2,12 +2,16 @@
 """ module load_from_json_file"""
 
 
+import json
+
+
 def load_from_json_file(filename):
-    """ 
+    """
     method load_from_json_file
     """
+
     content = ""
-    import json
+
     with open(filename, 'r', encoding="UTF-8") as file_name:
         content = file_name.read()
 

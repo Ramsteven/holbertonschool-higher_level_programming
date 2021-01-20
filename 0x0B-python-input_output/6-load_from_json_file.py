a0a0607cb24a5467f20deb0332592d6f8913ef35
ramsteven <ramsteven@gmail.com>
2021-01-20 23:46:30 +0000

    more changes about syntax

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-""" module load from json"""
+""" module load_from_json_file"""
 
 
 def load_from_json_file(filename):
-    """ method load from jsonfile"""
+    """ 
+    method load_from_json_file
+    """
     import json
-    with open(filename, encoding="utf-8") as file_name:
+    with open(filename, encoding="UTF-8") as file_name:
         return json.load(file_name)

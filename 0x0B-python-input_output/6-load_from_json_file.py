93ad1dec01fea59ac0dfc39dc390d1e2f1d1bafd
ramsteven <ramsteven@gmail.com>
2021-01-20 23:35:07 +0000

    delete unnecesary variable

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,4 @@ import json
 def load_from_json_file(filename):
     """ method load from """
     with open(filename, mode="r", encoding="utf-8") as file_name:
-        content = file_name.read()
-        return (json.loads(content))
+        return json.load(file_name)

4bb45195603cc8581fd53c44fb202aedd4fe0868
ramsteven <ramsteven@gmail.com>
2021-01-27 14:23:51 +0000

    adding comment and other things

--- 0x0C-python-almost_a_circle/models/validator.py
+++ 0x0C-python-almost_a_circle/models/validator.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
+""" module class validator """
+
 
 class Validator():
-    def compare(self, width, height, x, y): 
+    """ class check if the number received is correct"""
+    def compare(self, width, height, x, y):
         attr = [width, height, x, y]
         key = ["width", "height", "x", "y"]
-        
+
         for x in range(len(attr)):
             if isinstance(attr[x], int) is not True:
                 raise TypeError(key[x] + " must be an integer")
-        
+
         for x in range(len(attr[:2])):
             if attr[x] <= 0:
                 raise ValueError(key[x] + " must be > 0")
 
         for y in range(2, len(attr)):
-             if attr[y] < 0:
+            if attr[y] < 0:
                 raise ValueError(key[y] + " must be >= 0")
 
     def width_height(self, attribute, name):
+        """check if acessor methods received numbers ok """
         if isinstance(attribute, int) is not True:
             raise TypeError(name + " must be an integer")
 
         if attribute <= 0:
             raise ValueError(name + " must be > 0")
 
-    
     def x_y(self, attribute, name):
+        """check if acessor methods received numbers ok """
         if isinstance(attribute, int) is not True:
             raise TypeError(name + " must be an integer")
         if attribute < 0:

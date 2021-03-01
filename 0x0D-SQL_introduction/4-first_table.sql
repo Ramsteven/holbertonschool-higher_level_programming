6a45df754039458cb2ba53dd5c33f9fcd30eda60
ramsteven <ramsteven@gmail.com>
2021-03-01 14:39:28 -0500

    adding task

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- Querry to create a table gives DATABASE
 -- create table if dont exist 
-CREATE TABLE IF NOT EXIST firts_table (
+CREATE TABLE IF NOT EXIST first_table (
   id INT,
   name VARCHAR(256)
 );

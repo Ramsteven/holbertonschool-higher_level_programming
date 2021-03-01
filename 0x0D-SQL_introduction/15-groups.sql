166e5c4a1d27fb307719e1d007d8c77c271dd9b2
ramsteven <ramsteven@gmail.com>
2021-03-01 15:38:27 -0500

    fix name table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Query that allow filter the duplicates
 -- Show all duplicates with score and number
-SELECT score, count(score) as number FROM second_Table
+SELECT score, count(score) as number FROM second_table
        GROUP BY score
        ORDER BY score DESC;

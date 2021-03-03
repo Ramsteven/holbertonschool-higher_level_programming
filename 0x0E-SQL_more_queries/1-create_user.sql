bb61fd5a1f29b359c11a72903cd3f63657957675
ramsteven <ramsteven@gmail.com>
2021-03-02 21:29:03 -0500

    adding task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 --Query to create user
 -- this query create user with all privileges
+-- this query give privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

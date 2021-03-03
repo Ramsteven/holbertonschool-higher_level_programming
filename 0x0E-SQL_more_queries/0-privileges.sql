8dbbfd02436e57d2f6232bed6939dd2e07ac894d
ramsteven <ramsteven@gmail.com>
2021-03-02 21:16:25 -0500

    adding taks 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
---Query to list users
---This Query list all privileges of the follow users
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- Query to list users
+-- This Query list all privileges of the follow users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

617d01a8e0df82246b92a94902b07a0ef4e4974c
ramsteven <ramsteven@gmail.com>
2021-03-02 21:36:32 -0500

    adding sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
---Query to create user
--- this query create user with all privileges
--- this query give privileges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
+-- a script that creates the MySQL server user user_0d_1.
+-- user_0d_1 has all privileges on your MySQL server
+-- The user_0d_1 password is user_0d_1_pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

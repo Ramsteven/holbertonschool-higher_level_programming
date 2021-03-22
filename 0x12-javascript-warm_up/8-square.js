1d179c69edbd9b564f1b341ec0f407970ac08eaf
ramsteven <ramsteven@gmail.com>
2021-03-22 12:01:56 -0500

    adding other files

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+ square
+*/
+
+if (process.argv.length >= 3) {
+	const square = process.argv[2];
+	if (!isNaN(square)) {
+		for (let i = 0; i < parseInt(square); i++) {
+			console.log("X".repeat(square));
+		}
+	} else {
+		console.log("Missing size");
+	}
+} else {
+	console.log("Missing size");
+}

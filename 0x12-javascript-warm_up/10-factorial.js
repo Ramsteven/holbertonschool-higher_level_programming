6c77f34f0c484a20e463638c80550316eb96d30a
ramsteven <ramsteven@gmail.com>
2021-03-22 14:19:00 -0500

    adding task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,16 +3,16 @@
   factorial
 */
 
-function factorial(n) {
-	if (n === 0) {
-		return 1;
-	}
-	return n * factorial(n - 1);
+function factorial (n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
 if (isNaN(parseInt(process.argv[2]))) {
-	console.log(1);
+  console.log(1);
 } else {
-	const number = factorial(parseInt(process.argv[2]));
-	console.log(number);
+  const number = factorial(parseInt(process.argv[2]));
+  console.log(number);
 }

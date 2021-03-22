843e005ba16005f71e2027e9b606ae511f6e7e74
ramsteven <ramsteven@gmail.com>
2021-03-22 14:58:34 -0500

    solved variable

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-exports.callMeMoby = function callMeMoby(x, func) {
-	for (let i = 0; i < x; i++) {
-		fun();
-	}
+exports.callMeMoby = function callMeMoby (x, func) {
+  for (let i = 0; i < x; i++) {
+    func();
+  }
 };

ba671584dc86239f3b3d71da079e428476a36e8e
ramsteven <ramsteven@gmail.com>
2021-03-23 01:15:47 -0500

    adding task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+class Rectangle {
+	constructor(w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+
+	print() {
+		for (let y = 0; y < this.height; y++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
+
+	rotate() {
+		[this.width, this.height] = [this.height, this.width];
+	}
+
+	double() {
+		[this.width, this.height] = [this.width * 2, this.height * 2];
+	}
+}
+
+module.exports = Rectangle;

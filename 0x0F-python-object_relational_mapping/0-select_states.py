64cc9598b7f44f32e1ae1ab1652dda973af92108
ramsteven <ramsteven@gmail.com>
2021-03-27 20:50:49 +0000

    addin main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,19 @@ Module
 import MySQLdb
 from sys import argv
 
-if len(argv) is 4:
-    db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3],
-        port=3306)
+if __name__ == "__main__":
+    if len(argv) is 4:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            port=3306)
 
-    cursor = db.cursor()
+        cursor = db.cursor()
 
-    cursor.execute("SELECT id, name FROM states ORDER BY id;")
-    for id_query in cursor:
-        print("{}".format(id_query))
-    cursor.close()
-    db.close()
+        cursor.execute("SELECT id, name FROM states ORDER BY id;")
+        for id_query in cursor:
+            print("{}".format(id_query))
+        cursor.close()
+        db.close()

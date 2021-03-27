4a6ce66654a9c85fd3d9be56cf012a83cc1ecc77
ramsteven <ramsteven@gmail.com>
2021-03-27 20:14:20 +0000

    adding task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,6 +16,8 @@ if len(argv) is 4:
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-
-    for id_query in cursor:
-        print("{}".format(id_query))
+    rows = c.fetchall()
+    for eachRow in rows:
+        print(eachRow)
+    c.close()
+    conn.close() 

9ad628244dcbd3bfc298eba8dcac859bea61ec7d
ramsteven <ramsteven@gmail.com>
2021-03-27 21:00:16 +0000

    solver if

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,18 @@ Module
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    if len(argv) is 4:
-        db = MySQLdb.connect(
-            host='localhost',
-            user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            port=3306)
+db = MySQLdb.connect(
+    host='localhost',
+    user=argv[1],
+    passwd=argv[2],
+    db=argv[3],
+    port=3306)
 
-        cursor = db.cursor()
+cursor = db.cursor()
 
-        cursor.execute("SELECT * FROM states ORDER BY id;")
-        for id_query in cursor:
-            print("{}".format(id_query))
-        cursor.close()
-        db.close()
+cursor.execute("SELECT * FROM states ORDER BY id;")
+rows = cursor.fetchall()
+for id_query in rows:
+    print("{}".format(id_query))
+cursor.close()
+db.close()

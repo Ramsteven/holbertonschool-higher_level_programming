6aa8dece15ed8e0e36d84d184b16b7f1a70b7ad5
ramsteven <ramsteven@gmail.com>
2021-03-27 21:01:40 +0000

    adding main

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,19 @@ Module
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(
-    host='localhost',
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3],
-    port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        port=3306)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states ORDER BY id;")
-rows = cursor.fetchall()
-for id_query in rows:
-    print("{}".format(id_query))
-cursor.close()
-db.close()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    rows = cursor.fetchall()
+    for id_query in rows:
+        print("{}".format(id_query))
+    cursor.close()
+    db.close()

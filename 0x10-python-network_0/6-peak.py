5f72bbe5346a4df1504cc485e86f4456f81e36e6
ramsteven <ramsteven@gmail.com>
2021-04-08 22:42:48 -0500

    [UPDATE] changes name variables

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def find_peak(list_):
-    if list_ == []:
+def find_peak(list_of_integers):
+    if list_of_integers == []:
         return None
     else:
-        max_ = list_[0]
-        for item in list_:
-            if item > max_:
-                max_ = item
-        return (max_)
+        max_value = list_of_integers[0]
+        for item in list_of_integers:
+            if item > max_value:
+                max_value = item
+        return (max_value)
